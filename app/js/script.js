9e4accc33e0831427357e72a9e5320d30ba50758
ismailcandanX <ismailcandan44@gmail.com>
2022-04-14 02:35:39 +0300

    Animated Hamburger Menu.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header"); 
+const overlay = document.querySelector(".overlay"); 
+btnHamburger.addEventListener('click', function(){
+    if(header.classList.contains("open")){ // Close Hamburger Menu
+        header.classList.remove("open");
+        overlay.classList.add("fade-out");
+        overlay.classList.remove("fade-in");
+
+    } else{ // Open Hamburger Menu
+        header.classList.add("open");
+        overlay.classList.add("fade-in");
+        overlay.classList.remove("fade-out");
+    }
+})
