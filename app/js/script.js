346530c3f101a2b3cd6cc524d2f3fe3e51e11628
ismailcandanX <ismailcandan44@gmail.com>
2022-04-14 17:58:38 +0300

    Animated Hamburger Menu.

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,25 @@
-const btnHamburger = document.querySelector("#btnHamburger");
-const header = document.querySelector(".header"); 
-const overlay = document.querySelector(".overlay"); 
-btnHamburger.addEventListener('click', function(){
-    if(header.classList.contains("open")){ // Close Hamburger Menu
-        header.classList.remove("open");
-        overlay.classList.add("fade-out");
-        overlay.classList.remove("fade-in");
+const body = document.querySelector('body');
+const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
 
-    } else{ // Open Hamburger Menu
-        header.classList.add("open");
-        overlay.classList.add("fade-in");
-        overlay.classList.remove("fade-out");
-    }
+btnHamburger.addEventListener('click', function () {
+  if (header.classList.contains('open')) {
+    // Close Hamburger Menu
+    body.classList.remove("noscroll")
+    header.classList.remove('open')
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-in')
+      element.classList.add('fade-out')
+    })
+  } else {
+    // Open Hamburger Menu
+    body.classList.add("noscroll")
+    header.classList.add('open')
+    fadeElems.forEach(function (element) {
+      element.classList.add('fade-in')
+      element.classList.remove('fade-out')
+    })
+  }
 })
